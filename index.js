4cc7840ccf9007d5ff5469ae868316bbbf117df8
Le-vagabondev <75924500+Le-vagabondev@users.noreply.github.com>
2022-10-21 00:11:15 -0500

    secondCommit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const axios = require('axios');
 console.log('prueba node');
+
+
+let apiKey= 'ingresar Key';
+let city = 'Bogota, col';
+
+let requestconfig = {
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+
+}
+
+let objPromise = axios(requestconfig);
+
+objPromise.then((response)=>{console.log(response.data.main.temp -273.15 );}).catch((err)=>{console.log(err);});
